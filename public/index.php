24b72e6f29fb63bdf5a7edfa19661291321d3dce
Christian McDonald <cmcdonald@statesman.com>
2015-05-07 11:39:32 -0500

    header bottom margin fix and content entry

--- public/index.php
+++ public/index.php
@@ -91,8 +91,10 @@
         'step1' => 'Appraisal date',
         'step2' => 'Notices',
         'step3' => 'Protest deadline',
-        'step4' => 'Step 4'
-      ); ?>
+        'step4' => 'Step 4',
+        'step5' => 'Step 5',
+        'step6' => 'Step 6'
+        ); ?>
 
       <div id="progress-bar" class="col-xs-12">
         <div id="progress" class="container">
@@ -112,54 +114,99 @@
         </div>
       </div>
     </div>
-      <div id="step1" class="step row">
-        <div class="col-xs-12 col-sm-3 col-md-4">
-          <img class="img-responsive" src="http://placehold.it/600x400" />
-        </div>
-        <div class="col-xs-12 col-sm-9 col-md-8">
-          <h1>The appraisal base date</h1>
-          <h2>Jan. 1</h2>
-          <p>Your home’s appraised value for the year is based on its condition – and what the property could sell for – on this date. Keep in mind, the appraisal district arrives at a figure by calculating the market value of your property. And your home’s condition on Jan. 1 is key as you look at factors that may affect the value. Any damage or improvements after this date – say, a tree that falls on your roof in March, or the new bathroom you added in May – won’t affect the taxable value of your home this year.</p>
-        </div>
+    <div id="step1" class="step row">
+      <div class="col-xs-12 col-sm-3 col-md-4">
+        <img class="img-responsive" src="http://placehold.it/600x400" />
       </div>
-
-      <div id="step2" class="step row">
-        <div class="col-xs-12 col-sm-3 col-md-4">
-          <img class="img-responsive" src="http://placehold.it/600x400" />
-        </div>
-        <div class="col-xs-12 col-sm-9 col-md-8">
-          <h1>Appraisal notices arrive</h1>
-          <h2>Late April</h2>
-          <p>A piece of mail arrives from the appraisal district, telling you the value of your home for tax purposes. Look over the numbers carefully. If your property is not described correctly or if the value looks out of whack, you can protest it.</p>
-          <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of notices went to Travis County property owners in 2014.</p></div>
-        </div>
-      </div>
-
-      <div id="step3" class="step row">
-        <div class="col-xs-12 col-sm-3 col-md-4">
-          <img class="img-responsive" src="http://placehold.it/600x400" />
-        </div>
-        <div class="col-xs-12 col-sm-9 col-md-8">
-          <h1>Protest deadline</h1>
-          <h2></h2>
-          <p>This is the usual deadline for filing a “Notice of Protest” of your appraised value; since that date falls on a weekend this year, the notice can be filed on the next business day, which is Monday, June 1. What’s the best way to file? You have a couple of options. You can use the form on the back of the “Notice of Appraised Value” that you received from the appraisal district, since it already has your account information printed on it. Or you may be able to file your protest online. In Travis County, go to www.traviscad.org and click on E-File. Williamson County residents can go to www.wcad.org and click on “Online Protests,” while Hays County residents can do the same at www.hayscad.com . The benefit of online protesting is that appraisal district staffers can review your information and decide whether to offer you a settlement, potentially without you having to attend a hearing at all. [LINK to TCAD video explaining eFile process: http://www.traviscad.org/video/eFile/how_to_efile_player.html  ]</p>
-         </div>
-      </div>
-
-      <div id="step4" class="step row">
-        <div class="col-xs-12 col-sm-3 col-md-4">
-          <img class="img-responsive" src="http://placehold.it/600x400" />
-        </div>
-        <div class="col-xs-12 col-sm-9 col-md-8">
-          <h1>Step 4</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim mauris metus, ac placerat ligula sodales vitae. Quisque purus lectus, aliquet vel libero et, rhoncus efficitur massa. Sed commodo rhoncus fringilla. Aliquam vitae ex commodo, volutpat arcu mattis, dapibus dui. Proin ac aliquam augue, ac accumsan ligula. Morbi posuere turpis turpis, pharetra pulvinar neque interdum sit amet. In hac habitasse platea dictumst. Cras vehicula nibh ante, vel dignissim ex gravida a. Suspendisse vestibulum lorem arcu, ac blandit turpis tincidunt imperdiet. Sed elit mi, pharetra pretium enim id, dignissim porta justo. Sed eget maximus lorem, vel suscipit lorem. Vestibulum ut ante pulvinar, placerat purus tempor, ultricies quam.</p>
-          <p>Cras pretium interdum turpis at varius. Morbi tristique justo quam, molestie dapibus ante cursus non. Cras a nunc sed metus ornare mollis vel vel neque. Etiam suscipit sagittis sem, eu sodales eros maximus ac. Suspendisse vehicula pharetra erat, sit amet iaculis purus pharetra eget. Nulla facilisi. Quisque pretium massa et lorem ullamcorper hendrerit. Aliquam vel tristique mauris, ut bibendum magna. Nulla nec sagittis lectus, sed euismod lacus. Suspendisse vel elit risus. Proin eu ligula eleifend, aliquet nunc in, tincidunt ligula.</p>
-          <p>Phasellus lobortis nisi quis velit aliquet, a iaculis turpis vulputate. Ut quis faucibus nisl. Fusce quis augue lobortis, feugiat velit a, vulputate orci. In sit amet dolor ut erat dictum placerat. Sed ultricies justo et malesuada faucibus. Fusce erat velit, ornare non faucibus eget, fermentum vitae libero. Etiam cursus bibendum quam ac pharetra.</p>
-          <p>Duis tristique placerat sem, ut fermentum elit venenatis vitae. Suspendisse libero dui, tempor ac scelerisque id, lobortis ac velit. Curabitur augue risus, efficitur at orci in, tincidunt ornare diam. In fermentum justo id imperdiet pharetra. Nam dignissim sem id ante congue consectetur. Vestibulum elementum nisl vel luctus dictum. Cras eros est, cursus at quam eget, bibendum tempus quam. Ut pretium non velit id pretium. Fusce vel metus luctus, facilisis tellus finibus, egestas est. Mauris pulvinar ac mi id tristique. Sed dui mauris, egestas nec enim ac, tristique auctor orci. Phasellus eu mattis risus, vel sagittis purus. Nulla sed felis non lacus vestibulum elementum. Nam facilisis, leo ac molestie fermentum, sem leo tincidunt ligula, sit amet pharetra risus urna at turpis. Phasellus dapibus bibendum diam, nec pulvinar purus scelerisque at.</p>
-        </div>
+      <div class="col-xs-12 col-sm-9 col-md-8">
+        <h1>The appraisal base date</h1>
+        <h2>Jan. 1</h2>
+        <p>Your home’s appraised value for the year is based on its condition – and what the property could sell for – on this date. Keep in mind, the appraisal district arrives at a figure by calculating the market value of your property. And your home’s condition on Jan. 1 is key as you look at factors that may affect the value. Any damage or improvements after this date – say, a tree that falls on your roof in March, or the new bathroom you added in May – won’t affect the taxable value of your home this year.</p>
       </div>
     </div>
 
+    <div id="step2" class="step row">
+      <div class="col-xs-12 col-sm-3 col-md-4">
+        <img class="img-responsive" src="http://placehold.it/600x400" />
+      </div>
+      <div class="col-xs-12 col-sm-9 col-md-8">
+        <h1>Appraisal notices arrive</h1>
+        <h2>Late April</h2>
+        <p>A piece of mail arrives from the appraisal district, telling you the value of your home for tax purposes. Look over the numbers carefully. If your property is not described correctly or if the value looks out of whack, you can protest it.</p>
+        <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of notices went to Travis County property owners in 2014.</p></div>
+      </div>
+    </div>
+
+    <div id="step3" class="step row">
+      <div class="col-xs-12 col-sm-3 col-md-4">
+        <img class="img-responsive" src="http://placehold.it/600x400" />
+      </div>
+      <div class="col-xs-12 col-sm-9 col-md-8">
+        <h1>Protest deadline</h1>
+        <h2>May 31</h2>
+        <p>This is the usual deadline for filing a “Notice of Protest” of your appraised value; since that date falls on a weekend this year, the notice can be filed on the next business day, which is Monday, June 1. What’s the best way to file? You have a couple of options. You can use the form on the back of the “Notice of Appraised Value” that you received from the appraisal district, since it already has your account information printed on it. Or you may be able to file your protest online. In Travis County, go to <a href="http://www.traviscad.org"> traviscad.org</a> and click on E-File. Williamson County residents can go to <a href="www.wcad.org">wcad.org</a> and click on “Online Protests,” while Hays County residents can do the same at <a href="http://www.hayscad.com">hayscad.com</a>. The benefit of online protesting is that appraisal district staffers can review your information and decide whether to offer you a settlement, potentially <a href="http://www.traviscad.org/video/eFile/how_to_efile_player.html">without you having to attend a hearing at all</a>.</p>
+        <p>As you’re filling out the form, pay particular attention to the step in which you check off the box or boxes stating the reason for your protest. Your choices here will affect what kind of evidence you can present later on. If the district listed the wrong square footage for your home, for instance, be sure to check “property description is incorrect.” If you think your value is out of step with similar homes, make sure to mark “Value is unequal compared with other properties.”</p>
+        <p>For typical homeowners challenging their appraisal, the Texas Comptroller’s office suggests checking “Value is over market value” and “Value is unequal compared with other properties.” That will “allow you to present the widest types of evidence and preserve your full appeal rights,” the agency says in its handout on “<a href="http://www.window.state.tx.us/taxinfo/proptax/pdf/96-295.pdf">Property Taxpayer Remedies</a>.”</p>
+        <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of protests filed in Travis County in 2015, incl # of residential and # of business</p></div>
+       </div>
+    </div>
+
+    <div id="step4" class="step row">
+      <div class="col-xs-12 col-sm-3 col-md-4">
+        <img class="img-responsive" src="http://placehold.it/600x400" />
+      </div>
+      <div class="col-xs-12 col-sm-9 col-md-8">
+        <h1>Informal meeting</h1>
+        <h2>June 1</h2>
+        <p>The appraisal review board – or ARB, if you’re into acronyms – is already meeting on the first appeals through the gate. But we’re getting ahead of ourselves.</p>
+        <p>Once you’ve filed your “Notice of Protest,” the Travis Central Appraisal District will send you a letter with two dates: an informal meeting with an appraisal staffer and your formal hearing date with the ARB, a group of independent residents appointed to hear these challenges. During the informal meeting, the staffer will review the numbers with you. Bring all of your documentation: Information on comparable homes (records are available on the appraisal district’s website), perhaps an independent appraisal if you recently refinanced your house, or photos, repair estimates and other records showing damage that may devalue your home. Once you and a staffer have hashed it out, the district may offer to reduce your value by a certain amount. If you’re satisfied, you can accept it.</p>
+        <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of protests resolved through staff in 2015, total amount of those reductions and average reduction per home and per commercial site</p></div>
+        <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of protests dropped by property owners in 2015</p></div>
+        <p>If not, you can keep your date with the ARB, or technically, with a three-member panel of ARB members. Before that hearing, you have a right to see all of the information the district appraisers plan to present, so be sure to contact the appraisal district to request those documents.</p>
+        <p>In Travis County, typically two weeks pass between the informal meeting with staff and the ARB hearing. In Williamson and Hays counties, that meeting with a staffer usually happens the same day as the ARB hearing. Either way, the same process applies: If you’re satisfied with the outcome after meeting with the staffer, you can forego the ARB hearing.</p>
+       </div>
+    </div>
+
+    <div id="step5" class="step row">
+      <div class="col-xs-12 col-sm-3 col-md-4">
+        <img class="img-responsive" src="http://placehold.it/600x400" />
+      </div>
+      <div class="col-xs-12 col-sm-9 col-md-8">
+        <h1>The big day</h1>
+        <p>If you take your case to the ARB, come prepared and expect a rapid-fire proceeding. The entire hearing will likely take 15 to 30 minutes. In that time you will be placed under oath and given a chance to present any evidence or witnesses supporting your case. You must conclude by stating the figure you believe your property is worth. Someone from the appraisal district will likely question you and provide additional evidence. Then you can question the appraiser or any witnesses presented by the appraisal district. Members of the ARB can ask clarifying questions, too. Finally, each side gets to make a closing statement, so once again you’ll want to reiterate what you believe your property is worth and why. The three-member panel will discuss the case and reach a recommended value.</p>
+        <p><span>The big day:</span> The full ARB will review the recommendation of the three-person panel and approve your final assessed value. You’ll get a certified letter in the mail with the decision.</p>
+        <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of protests heard by ARB panels in 2015, total dollar amount reduction for those properties, average reduction per home and per commercial site</p></div>
+       </div>
+    </div>
+
+    <div id="step6" class="step row">
+      <div class="col-xs-12 col-sm-3 col-md-4">
+        <img class="img-responsive" src="http://placehold.it/600x400" />
+      </div>
+      <div class="col-xs-12 col-sm-9 col-md-8">
+        <h1>Wrapping up</h1>
+        <h2>July 20</h2>
+        <p>Protest hearings typically wrap up by now. But you still have recourse if you’re not satisfied with the ARB decision. If your property is valued at less than $1 million, or if it’s your homestead, regardless of value, you can take your case to binding arbitration. There’s a <a href="http://www.window.state.tx.us/taxinfo/taxforms/ap-219.pdf">form for that</a>, too. You’ll need to file that within 45 days of receiving the ARB decision, and it’ll cost you $500. All but $50 of that will be refunded if you prevail.</p>
+        <p>There are two alternatives to arbitration. You can take your appeal to state district court (that challenge must be filed within 60 days of receiving the ARB decision, and you’ll likely need an attorney’s help). Or for properties valued over $ 1 million, you can file an appeal with the State Office of Administrative Hearings (that challenge must be filed within 30 days of receiving the ARB decision). </p>
+        <div class="well"><i class="fa fa-briefcase fa-5x"></i><p><strong>By the numbers</strong></p><p>NUMBER of Travis County cases that sought arbitration in 2015; do we have any outcome data for these cases? </p></div>
+       </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12">
+        <h2>Rules of the road</h2>
+        <ul>
+          <li>DON’T: Complain about how you can’t afford your taxes. The appraisal district doesn’t set the tax rate; they only decide what your home is worth for tax purposes.</li>
+          <li>DO: Come armed with a specific value you believe your home is worth, and the documentation to support that figure.</li>
+          <li>DON’T: Give a long, rambling presentation filled with generalizations.</li>
+          <li>DO: Request in advance the documentation the appraisal district compiled to calculate its appraisal of your property.  Examine the comparable homes used to determine your property’s value. See whether differences in property size, home upgrades or proximity to a busy road, for instance, might make those properties a poor comparison for your home.</li>
+        </ul>
+        <p>Sources: Travis Central Appraisal District; Texas Comptroller’s Office.</p>
+      </div>
+    </div>
+
+
+<!-- there was a close div here at one time -->
   </article>
 
   <!-- bottom matter -->
